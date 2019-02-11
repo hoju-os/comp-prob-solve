0ad6b3abf586129c24834e9eb892ed5a5ce174ae
hoju-os <47367882+hoju-os@users.noreply.github.com>
2019-02-11 11:42:07 -0500

    Add files via upload

---
+++ lkj.py
@@ -0,0 +1,12 @@
+decimalNum = 255
+baseNum = 16
+quotient = 1
+answer = ""
+remainder = 0
+while(quotient != 0):
+    quotient = int (decimalNum/baseNum)
+    remainder = decimalNum%baseNum
+    answer = str(remainder)+ str(answer)
+    decimalNum = quotient
+print(answer)
+    
